3cf978c2c49e44d6f299f2b7ecd533f1aa733dcb
victoryanonymous <victoryvinayanonymous@gmail.com>
2024-01-24 22:11:35 +0530

    delected images

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
-
+import WriteEase from '../assets/logo.png';
+import Contact from '../assets/Contact.png';
+import HowWorks from '../assets/How it Works.png';
+import Pricing from '../assets/Pricing.png';
+import UseCases from '../assets/Use Cases.png';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <img src={WriteEase} alt="WriteEase" />
     </div>
   )
 }
