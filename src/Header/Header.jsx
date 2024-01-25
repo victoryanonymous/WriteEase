5373977bf867f3cf076438bb7a74cf5e65d7ce19
Vinay Maggidi <111217552+victoryanonymous@users.noreply.github.com>
2024-01-25 23:00:48 +0530

    Update Header.jsx

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -4,7 +4,7 @@ import GallaryImage from './GallaryImage';
 export default function Header() {
     const images = [
         '../assets/logo.png',
-        // '../assets/Use Cases.png'
+        '../assets/Use Cases.png'
     ];
     return (
         <div className="header">
